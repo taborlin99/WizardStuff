6effc491eef5404db60c1a844c3a07822b68d018
Yahoo <noapeterson@gmail.com>
2024-06-14 10:59:42 -0500

    Visuals, pathfinding
    added placeholder graphics, messed around with particle systems, and added a GOBLIN.

--- scripts/entities/projectileentity.gd
+++ scripts/entities/projectileentity.gd
@@ -30,7 +30,6 @@ func load_spell(spell_array, index):
 	scenenode.index = index + 1
 	scenenode.spell_array = spell_array
 	get_node("/root").add_child(scenenode)
-	print(index)
 	return scenenode
 	
 	
