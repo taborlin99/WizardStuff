a7e635c07875941ce96e0b9cd4152e8b1683c79f
Yahoo <noapeterson@gmail.com>
2024-06-14 00:36:24 -0500

    Spell_tweaking
    modified spell system, added more functions to spell_entity class

--- scenes/spells/bolt/bolt.gd
+++ scenes/spells/bolt/bolt.gd
@@ -1,25 +1,8 @@
 extends Projectile
-	
-func _ready():
-	pass
-	
+
+
 func _physics_process(delta):
-	local_rotation = Vector2.RIGHT.rotated(rotation)
-	velocity = velocity.move_toward(local_rotation * max_speed, acceleration)
+	standard_movement()
 	move_and_slide()
-	lifetime -= delta
-	if lifetime <= delta:
-		projectile_expired()
-	
-func projectile_expired():
-	print("free")
-	if index < spell_array.size():
-		cast_spell_chain()
-	print(index)
-	print(spell_array.size())
-	queue_free()
+	track_lifetime(delta)
 		
-func cast_spell_chain():
-	var sp1 = load_spell(spell_array, index + 1)
-	sp1.global_position = self.global_position
-	print("test")
--- scripts/entities/projectileentity.gd
+++ scripts/entities/projectileentity.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 class_name Projectile
 
+var spell_ready : bool
 @onready var index : int
 @onready var spell_array : Array
-@onready var local_rotation : Vector2
-
+@onready var local_rotation : Vector2 
 @export var initial_speed : float
 @export var max_speed : float
 @export var acceleration : float
@@ -17,12 +17,35 @@ class_name Projectile
 @export var peircing : bool
 
 
+func init():
+	velocity = (Vector2.RIGHT.rotated(rotation) * initial_speed)
+	
+func standard_movement():
+	local_rotation = Vector2.RIGHT.rotated(rotation)
+	velocity = velocity.move_toward(local_rotation * max_speed, acceleration)
+	
 func load_spell(spell_array, index):
-	self.index = index
-	self.spell_array = spell_array 
 	var scene = load("res://scenes/spells/" + spell_array[index] + "/" + spell_array[index] + ".tscn")
 	var scenenode = scene.instantiate()
-	scenenode.index = index
+	scenenode.index = index + 1
 	scenenode.spell_array = spell_array
 	get_node("/root").add_child(scenenode)
+	print(index)
 	return scenenode
+	
+	
+func cast_spell_chain():
+	if index < spell_array.size():
+		var spell = load_spell(spell_array, index)
+		spell.global_position = self.global_position
+		spell.rotation = self.global_rotation
+		spell.init()
+		
+func projectile_expired():
+	cast_spell_chain()
+	queue_free()
+
+func track_lifetime(delta):
+	lifetime -= delta
+	if lifetime <= 0:
+		projectile_expired()
