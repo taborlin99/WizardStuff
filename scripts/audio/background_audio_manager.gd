4561ce3ec759ebde695726c0ffc997a72acb7a52
Lukas <lpetersonrhs@gmail.com>
2024-06-17 21:48:22 -0500

    present 4 u

--- scripts/audio/background_audio_manager.gd
+++ scripts/audio/background_audio_manager.gd
@@ -1,11 +1,14 @@
 extends Node
 
 @onready var test = $test
+@onready var bg_music = $bg_music
+
 
 var background_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	bg_music.play()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
